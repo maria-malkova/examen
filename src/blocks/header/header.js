74ed5a1ab4b7ce2a3f4b244d249da8ddffc4e5da
Maria Malkova <79992031684@yandex.ru>
2023-08-07 00:37:57 +0300

    Топорные кужочки

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,31 +1,26 @@
-// // Выбираем кнопку
-// const btn = document.querySelector(".header__symbol");
-// // Выбираем элемент link со стилями темы
-// const theme = document.querySelector("#theme-link");
-
-// // Ловим нажатие на кнопку
-// btn.addEventListener("click", function() {
-//   // Если в URL "ligh-theme.css"
-//   if (theme.getAttribute("href") == "css/style.css") {
-//     // ... переключаем на "dark-theme.css"
-//     theme.href = "css/dark-theme.css";
-//   // Иначе...
-//   } else {
-//     // ... переключаем на "light-theme.css"
-//     theme.href = "css/style.css";
-//   }
-// });
-
-
 // Находим нужную кнопку
 const btn = document.querySelector('.header__symbol');
-
 // Прослушиваем нажатие на кнопку
 btn.addEventListener('click', function() {
     // Переключаем (добавляем/удаляем) класс .dark-theme
     document.body.classList.toggle('dark-theme'); 
-    document.header.classList.toggle('dark-theme'); // не работает
-
 })
+
+$(document).ready(function (){
+    $('.respect__name').click(function(){
+        if ($(this).siblings('.respect__text').hasClass('respect__text--hide')){
+            $('.respect__text').removeClass('respect__text--hide');
+            $(this).siblings('.respect__text').removeClass('respect__text--hide');
+            $('.respect__name').removeClass('respect__name--hide');
+            $(this).removeClass('respect__name--hide');
+        } else {
+            $(this).siblings('.respect__text').addClass('respect__text--hide');
+            $(this).addClass('respect__name--hide');
+        }
+    })
+})
+
+
+
 
 
